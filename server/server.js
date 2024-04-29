e33b3decc4153f0e792d9d3afaacf5e265cd080e
laxman1892 <lxmnrzl@gmail.com>
2024-04-29 20:43:33 +0545

    added dependency

--- server/server.js
+++ server/server.js
@@ -3,8 +3,16 @@ const io = require('socket.io');
 const http = require('http');
 const mongoose = require('mongoose');
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
+// ! enabling cors and json parsing
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use(cookieParser());
 
 const server = http.createServer(app);
 
@@ -17,5 +25,9 @@ async function main() {
 
 const db = mongoose.connection;
 db.once("connected", () => console.log("Connected to MongoDB!"));
+// ---------------------------------------------------------------------------------
+
+const salt = bcrypt.genSaltSync(10); // salt for password hashing
+const jwtSalt = "lshjogihqwoihq209u523h5klhdklhfowieh"; //jwt salt for signing tokens
 
 server.listen(3001, () => { console.log("Listening on port 3001"); });
